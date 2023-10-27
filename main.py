ea3829e2b7d38d3c8cd6a8f7bc5045163ffa1f52
mohammadattaibrahim <moh.ibrahimm95@gmail.com>
2023-10-27 15:40:17 +0300

    create function display menu

---
+++ main.py
@@ -0,0 +1,10 @@
+name=input("Enter your name :")
+print ("your welcomr ",name)
+def displayMenu():
+  print ("1. Add Matrices\n" +
+         "2. Check Rotation\n" +
+         "3. Invert Dictionary\n" +
+        "4. Convert Matrix to Dictionary\n" +
+         "5. Check Palindrome\n" +
+         "6. Search for an Element & Merge Sort\n" +
+         "7. Exit\n")
